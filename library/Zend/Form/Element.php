02848d0d41950ea866307ff83ab87e833bf9d1ca
Charitha Ratnayaka <charitha.ratnayaka@cms.lk>
2026-05-14 10:10:26 +0530

    PHP 8.2 compatibility updates

--- library/Zend/Form/Element.php
+++ library/Zend/Form/Element.php
@@ -40,6 +40,7 @@
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id$
  */
+#[AllowDynamicProperties]
 class Zend_Form_Element implements Zend_Validate_Interface
 {
     /**
@@ -60,6 +61,8 @@ class Zend_Form_Element implements Zend_Validate_Interface
     public $class;
 
     public $type;
+
+    public $disable;
 
     /**
      * 'Allow empty' flag
